6373c7c135064ff216b78fc5c1b47fb33ebafd18
Meridja Abdellahnassim <abdellah.meridja@univ-lr.fr>
2022-04-22 17:20:57 +0200

    routes changes

--- backend-system/api-gateway/routes.js
+++ backend-system/api-gateway/routes.js
@@ -1,44 +1,15 @@
 const ROUTES = [
   {
-    url: '/decision_maker',
+    url: '/slices',
     proxy: {
       target: 'http://decision_maker',
       changeOrigin: true,
       pathRewrite: {
-        [`^/decision_maker`]: '',
+        [`^/slices`]: '',
       },
     },
   },
-  {
-    url: '/data_contextualize',
-    proxy: {
-      target: 'http://data_contextualize',
-      changeOrigin: true,
-      pathRewrite: {
-        [`^/data_contextualize`]: '',
-      },
-    },
-  },
-  {
-    url: '/intent_monitor',
-    proxy: {
-      target: 'http://intent_monitor',
-      changeOrigin: true,
-      pathRewrite: {
-        [`^/intent_monitor`]: '',
-      },
-    },
-  },
-  {
-    url: '/policy_generator',
-    proxy: {
-      target: 'http://policy_generator',
-      changeOrigin: true,
-      pathRewrite: {
-        [`^/policy_generator`]: '',
-      },
-    },
-  },
+  
 ];
 
 exports.ROUTES = ROUTES;
